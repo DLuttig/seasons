0bd90de17f3eb724882c471918672e9f08726a4b
dluttig <dluttig5@live.com>
2019-03-24 11:00:23 +1300

    first commit

--- src/components/SeasonDisplay.jsx
+++ src/components/SeasonDisplay.jsx
@@ -4,13 +4,6 @@ import ReactDOM from 'react-dom'
 const App = () => {
   window.navigator.geolocation.getCurrentPosition(
     (position) => console.log(position), (err) => console.log(err)
-  )
-    return (
-      <div></div>
-    )
-}
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-)
+  )
+}
